031b36ac92bf0d469fb67455d09bed7166b5683d
Kim sung hyun <wellma10@naver.com>
2021-06-18 10:08:05 +0900

    보드 조회수, 좋아요기능 추가중

--- src/main/java/org/wine/mapper/ReplyMapper.java
+++ src/main/java/org/wine/mapper/ReplyMapper.java
@@ -8,7 +8,7 @@ import org.wine.domain.ReplyVO;
 
 public interface ReplyMapper {
 	public int insert(ReplyVO vo);
-	public ReplyVO read(Long boardNum);
+	public ReplyVO read(Long rno);
 	public int delete(Long rno);
 	public int update(ReplyVO reply);
 	
